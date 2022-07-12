12a4470edc70963d4694b6cf9966201edfeaf7dc
MaevaWolff <maeva.wolff@hetic.net>
2022-07-12 17:32:51 +0200

    feat: add Button base component

---
+++ src/components/base/Button.tsx
@@ -0,0 +1,37 @@
+import React, { PropsWithChildren } from "react";
+import { Link, LinkProps } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+export type ButtonProps = PropsWithChildren<{} & LinkProps>;
+
+// TODO: Add outlined variant
+export default function Button({ children, ...rest }: ButtonProps) {
+  // NOTE: It's commented while waiting to see if we need a button
+  //       with click events and not a redirection to a page.
+  //
+  //   if ("to" in rest) {
+  //     return <StyledLink {...rest}>{children}</StyledLink>;
+  //   } else {
+  //     return <StyledButton>{children}</StyledButton>;
+  //   }
+
+  return <StyledLink {...rest}>{children}</StyledLink>;
+}
+
+const buttonStyles = css`
+  all: unset;
+  box-sizing: border-box;
+  width: fit-content;
+  background-color: ${({ theme }) => theme.colors.brand.primary.base};
+  color: ${({ theme }) => theme.colors.core.white.base};
+  border-radius: ${({ theme }) => theme.border.radius.md};
+  padding: ${({ theme }) => theme.spacing[5]} ${({ theme }) => theme.spacing[6]};
+`;
+
+const StyledLink = styled(Link)`
+  ${buttonStyles}
+`;
+
+// const StyledButton = styled.button`
+//   ${buttonStyles}
+// `;
