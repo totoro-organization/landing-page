6153fb8b6ec9352fae3efac320ff35325fa5f351
MaevaWolff <maeva.wolff@hetic.net>
2022-07-12 20:29:13 +0200

    feat: add logo organization partners

--- src/components/sections/OrganizationPartners/OrganizationPartners.tsx
+++ src/components/sections/OrganizationPartners/OrganizationPartners.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import EmmausSolidarite from "../../../assets/png/logo/EmmausSolidarite.png";
+import PetitsFreresDesPauvres from "../../../assets/png/logo/PetitsFreresDesPauvres.png";
+import SecoursPopParis from "../../../assets/png/logo/SecoursPopParis.png";
+import ReglesElementaires from "../../../assets/png/logo/ReglesElementaires.png";
+import { device } from "../../../theme/devices";
+
+const organizationLogos = [
+  {
+    logo: EmmausSolidarite,
+    alt: "Emmaus logo",
+  },
+  {
+    logo: PetitsFreresDesPauvres,
+    alt: "Petits freres des pauvres logo",
+  },
+  {
+    logo: SecoursPopParis,
+    alt: "Secours populaire de paris logo",
+  },
+  {
+    logo: ReglesElementaires,
+    alt: "Règles élementaires logo",
+  },
+];
 
 export default function OrganizationPartners() {
   return (
     <Container>
       <h2>Les associations qui nous font confiance</h2>
 
-      {/* TODO: Add logo partners */}
       <PartnersList>
-        <div
-          style={{ width: "8rem", height: "3rem", backgroundColor: "grey" }}
-        />
-        <div
-          style={{ width: "8rem", height: "3rem", backgroundColor: "grey" }}
-        />
-        <div
-          style={{ width: "8rem", height: "3rem", backgroundColor: "grey" }}
-        />
-        <div
-          style={{ width: "8rem", height: "3rem", backgroundColor: "grey" }}
-        />
-        <div
-          style={{ width: "8rem", height: "3rem", backgroundColor: "grey" }}
-        />
+        {organizationLogos.map(({ logo, alt }) => (
+          <img src={logo} alt={alt} />
+        ))}
       </PartnersList>
     </Container>
   );
@@ -45,5 +56,10 @@ const Container = styled.section`
 const PartnersList = styled.div`
   display: flex;
   flex-wrap: wrap;
+  align-items: center;
   gap: 1rem;
+
+  @media ${device.laptop} {
+    gap: 4rem;
+  }
 `;
